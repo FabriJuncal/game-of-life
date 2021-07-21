1b24f8e74a4f5c648fbd62eebd7f3113f8017c87
FabriJuncal <juncalfabri@gmail.com>
2021-07-20 23:32:27 -0300

    Primer commit - Limpieza de Archivos - Se creó la grilla

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+// Se asigna el tamaño de la grilla
+const nroColumnas = 50;
+const nroFilas = 30
+
+
 
 function App() {
+
+// =========================================================================
+  /* Este código crea las Columnas y Filas */
+
+  const filas = [];
+  // Iteramos sobre la cantidad de Filas para ir 
+  for (let i = 0; i < nroFilas; i++){
+
+    // Creamos la cantidad de columnas asignadas con el valor 0
+    let arrayColumnas = Array.from(Array(nroColumnas), () => 0);
+    // Cargamos las Columnas dentro de cada Fila
+    filas.push(arrayColumnas);
+  }
+// =========================================================================
+  const [grilla, setGrilla] = useState(filas);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      // Utilizamos Grid para repetir/crear la cantidad de Columnas que se asignó
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${nroColumnas}, 20px)`
+      }}
+    >
+      {/* Iteramos sobre los array's definidos para crear cada una de las Celulas */}
+      {grilla.map((filas, i) => 
+        filas.map((columnas, j) => (
+          <div
+            key={`${i}-${j}`}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grilla[i][j] ? "blue" : undefined,
+              border: "solid 1px black"
+            }}
+          />
+        ))
+      )}
     </div>
   );
 }
