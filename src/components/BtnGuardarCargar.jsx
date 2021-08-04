b32f45fdf70ebdc495a748d2f1789814c9b5207b
FabriJuncal <juncalfabri@gmail.com>
2021-08-04 02:36:21 -0300

    Fix Comentario

--- src/components/BtnGuardarCargar.jsx
+++ src/components/BtnGuardarCargar.jsx
@@ -1,6 +1,11 @@
 /*Componente Personalizado para Cargar o Guardar las Grillas en el LocalStorage*/
 import Button from '@material-ui/core/Button';
 
+/*
+ Componente de los botones de Guardar/Cargar.
+ Este identificará si cada Slot contiene una grilla almacenada en el Local Storage y según lo que encuentre, mostrará la función disponible a ejecutar
+*/
+
 const BtnGuardarCargar = ({nroGrilla, grillaSeleccionado, actGrillaSeleccionado, guardarCargarGrilla, actualizarRecorrido}) => {
   const grillaGuardada = JSON.parse(localStorage.getItem('grilla'+nroGrilla)) ? JSON.parse(localStorage.getItem('grilla'+nroGrilla)) : [];
   let operacion, texto;
